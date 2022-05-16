deb6bd101c247d89fedb8ae5847931dcfb53c9bd
Yhames <wjddn494@naver.com>
2022-05-16 22:35:07 +0900

    intellij commit test

---
+++ member-management/src/main/java/hello/membermanagement/service/MemberService.java
@@ -0,0 +1,37 @@
+package hello.membermanagement.service;
+
+import hello.membermanagement.domain.Member;
+import hello.membermanagement.repository.MemberRepository;
+import hello.membermanagement.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {   // 값이 존재하는지 확인하는 Optional의 메서드
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // id로 조회
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
