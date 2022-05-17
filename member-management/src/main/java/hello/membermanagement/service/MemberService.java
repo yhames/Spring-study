d575ba9ed7e47af3ffc1a59ea7037597000dfc85
Yhames <wjddn494@naver.com>
2022-05-17 09:00:36 +0900

    intellij commit

--- member-management/src/main/java/hello/membermanagement/service/MemberService.java
+++ member-management/src/main/java/hello/membermanagement/service/MemberService.java
@@ -8,7 +8,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // Dependency Injection
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    // TODO: DI 정리
 
     // 회원가입
     public Long join(Member member) {
@@ -31,7 +37,7 @@ public class MemberService {
     }
 
     // id로 조회
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
---
+++ member-management/src/test/java/hello/membermanagement/service/MemberServiceTest.java
@@ -0,0 +1,71 @@
+package hello.membermanagement.service;
+
+import hello.membermanagement.domain.Member;
+import hello.membermanagement.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach  //TODO: 이게 왜 문제가 되는건지 다시 확인
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given : 이런 상황에서
+        Member member = new Member();
+        member.setName("spring");
+
+        // when : 이것을 실행했을 때
+        Long saveId = memberService.join(member);
+
+        // then : 이런 결과가 나와야한다.
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void duplicateMemberException() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
