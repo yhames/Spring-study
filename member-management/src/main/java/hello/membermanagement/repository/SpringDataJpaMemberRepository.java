f7c8e7b97ae64d9f27dba654812d54bb49749ce5
Yhames <wjddn494@naver.com>
2022-05-18 22:34:59 +0900

    intellij commit

---
+++ member-management/src/main/java/hello/membermanagement/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.membermanagement.repository;
+
+import hello.membermanagement.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);   // JPQL select m from Member m where m.name = ?
+}
--- member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
+++ member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
@@ -10,11 +10,18 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    //    private final EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private final DataSource dataSource;
 //
@@ -25,14 +32,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
