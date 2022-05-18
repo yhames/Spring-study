48a3300a324e8434c7102dccaabf1889a1ac9967
Yhames <wjddn494@naver.com>
2022-05-18 19:09:32 +0900

    intellij commit

--- member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
+++ member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.membermanagement.service;
 
+import hello.membermanagement.repository.JdbcMemberRepository;
 import hello.membermanagement.repository.MemberRepository;
 import hello.membermanagement.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
