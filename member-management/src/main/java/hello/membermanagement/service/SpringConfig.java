a73fcfe6311c6037728d7b70d62724ced34abbfe
Yhames <wjddn494@naver.com>
2022-05-18 23:40:37 +0900

    intellij commit

--- member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
+++ member-management/src/main/java/hello/membermanagement/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.membermanagement.service;
 
+import hello.membermanagement.aop.TimeTraceAop;
 import hello.membermanagement.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +32,7 @@ public class SpringConfig {
 //    }
 
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository);
     }
 
