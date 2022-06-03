a536ea6178f2486e854027e7560362a60a359dad
Yhames <wjddn494@naver.com>
2022-06-03 22:43:11 +0900

    jpa and db config

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional  // 테스트에 있으면 테스트가 끝나고 롤백
+//    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        //영속성 컨텍스트에서 식별자가 같으면 동일한 엔티티로 인식한다.
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("(findMember == member) = " + (findMember == member));
+    }
+}
