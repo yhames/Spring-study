b7e0212dc63d5adee4ea17b98bc92c356e0a2b1f
Yhames <wjddn494@naver.com>
2022-06-09 14:48:19 +0900

    domain, repository

---
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,42 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되지 않음, 실무에서는 ConcurrentHashMap, AtomicLong을 고려
+ */
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    private MemberRepository() {
+    }
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> finaAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
