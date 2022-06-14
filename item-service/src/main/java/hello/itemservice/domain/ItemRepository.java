144096c3bac007f43c6ae93bfe036e5af5c74500
Yhames <wjddn494@naver.com>
2022-06-14 22:21:41 +0900

    item domain, repository

---
+++ item-service/src/main/java/hello/itemservice/domain/ItemRepository.java
@@ -0,0 +1,40 @@
+package hello.itemservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();   //concurrentHashMap
+    private static long sequence = 0L; // atomic long
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long id, Item updateParam) { //ItemParamDto
+        Item findItem = findById(id);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {  //for Test
+        store.clear();
+    }
+}
