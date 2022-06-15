cc66e61589344073615c86c3e5c91fab7ca56f93
Yhames <wjddn494@naver.com>
2022-06-16 01:01:11 +0900

    thymeleaf-fin

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,9 +5,8 @@ import hello.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -32,8 +31,89 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
     /**
-     * 테스트용 데이터 추ㅓ
+     * @ModelAttribute
+     * 1. 요청파라미터 처리: 객체를 생성하고, 프로퍼티 접근법으로 값을 입력
+     * 2. Model 추가: 생성한 객체를 Model에 자동으로 넣음
+     */
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item);   //자동추가, 생략가능
+        return "basic/item";
+    }
+
+    /**
+     * Model 이름 생략시 클래스명 카멜형식으로 지정
+     */
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * 객체 파라미터인 경우 @ModelAttribute 자동 적용
+     * 기본형 파라미터인 경우 @RequestParam 자동 적용
+     */
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";    //새로고침시 중복 등록!!
+    }
+
+    /**
+     * PRG(Post/Redirect/)
+     */
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+//        return "basic/item";    //새로고침시 중복 등록!!
+        return "redirect:/basic/items/" + item.getId(); //URL인코딩 안됨!
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());   //PathVariable
+        redirectAttributes.addAttribute("status", true);    //QueryParameter
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init() {
