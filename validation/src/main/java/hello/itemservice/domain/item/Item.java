ceaec35e821295ad63147707492998607bb685ce
Yhames <wjddn494@naver.com>
2022-07-17 18:10:40 +0900

    bean validation and api validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,17 +1,51 @@
 package hello.itemservice.domain.item;
 
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000",
+//        message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
     }
+
+    public Item(ItemSaveForm form) {
+        this.itemName = form.getItemName();
+        this.price = form.getPrice();
+        this.quantity = form.getQuantity();
+    }
+
+    public Item(ItemUpdateForm form) {
+        this.itemName = form.getItemName();
+        this.price = form.getPrice();
+        this.quantity = form.getQuantity();
+    }
+
+
+
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
---
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min=1000, max=1000000)
+    private Integer price;
+
+    @NotNull
+    private Integer quantity;
+}
